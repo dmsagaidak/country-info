74e9cd0e75a906a47ccf10145f5c840491348335
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2022-11-22 22:16:20 +0600

    Created structure of the app: added .idea to .gitignore created Countryinfo.tsx, SelectCountry.tsx, types.d.ts

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SelectCountry from "../../components/SelectCountry/SelectCountry";
+import CountryInfo from "../../components/CountryInfo/CountryInfo";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SelectCountry/>
+      <CountryInfo/>
+    </div>
+  );
+}
+
+export default App;
