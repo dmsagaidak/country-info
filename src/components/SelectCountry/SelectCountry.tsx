2fa07d20fc5c16395f5abacc1cf94971e839b881
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2022-11-23 07:32:31 +0600

    Modified SelectCountry.tsx Added onChange attribute, onSelect function

--- src/components/SelectCountry/SelectCountry.tsx
+++ src/components/SelectCountry/SelectCountry.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
+import axios from "axios";
+import {ApiCountry} from "../../types";
 
-const SelectCountry = () => {
+const COUNTRIES_URL = 'https://restcountries.com/v2/all?fields=alpha3Code,name';
+
+interface Props {
+  onSelect: () => void
+}
+
+const SelectCountry: React.FC<Props> = ({onSelect}) => {
+  const [countries, setCountries] = useState<ApiCountry[]>([]);
+  const [currentCountry, setCurrentCountry] = useState<ApiCountry>({
+    name: '',
+    alpha3Code: ''
+  })
+
+  const fetchCountry = useCallback(async () => {
+    const countryResponse = await axios.get<ApiCountry[]>(COUNTRIES_URL);
+    setCountries(countryResponse.data)
+
+  }, []);
+
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const {name, value} = e.target;
+    setCurrentCountry(prev =>({...prev, [name]: value}))
+  }
+
+  useEffect(() =>{
+    fetchCountry().catch(console.error);
+  }, [fetchCountry]);
+
   return (
     <div>
-
+      <select
+        name="name"
+        value={currentCountry.name}
+        onChange={onChange}
+        onSelect={onSelect}
+      >
+        <option disabled value=''>Select a country</option>
+        {countries.map(country => (
+          <option
+            key={country.alpha3Code}
+            value={country.name}
+          >{country.name}</option>
+        ))}
+      </select>
     </div>
   );
 };
